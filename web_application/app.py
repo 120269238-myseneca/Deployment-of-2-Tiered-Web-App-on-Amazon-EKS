35df19af352a8891de6ef37e35cd2a1f8a422762
Sudeep Saurabh <ssaurabh3@myseneca.ca>
2024-03-12 14:59:23 -0400

    I edit to have image+groupname with app edit as well

--- web_application/app.py
+++ web_application/app.py
@@ -3,16 +3,21 @@ from pymysql import connections
 import os
 import random
 import argparse
+import boto3
+
 
 
 app = Flask(__name__)
 
-DBHOST = os.environ.get("DBHOST") or "localhost"
-DBUSER = os.environ.get("DBUSER") or "root"
-DBPWD = os.environ.get("DBPWD") or "passwors"
-DATABASE = os.environ.get("DATABASE") or "employees"
-COLOR_FROM_ENV = os.environ.get('APP_COLOR') or "lime"
-DBPORT = int(os.environ.get("DBPORT"))
+DBHOST = os.environ.get("DBHOST", "localhost")
+DBUSER = os.environ.get("DBUSER", "root")
+DBPWD = os.environ.get("DBPWD", "password") 
+DATABASE = os.environ.get("DATABASE", "employees")
+DBPORT = int(os.environ.get("DBPORT", 3306)) 
+bucketname = os.environ.get("bucketname", "clo835group1")
+bgimage = os.environ.get("backgroundimage", "project1.jpg")
+groupName = os.environ.get("groupName", "Group-1")
+
 
 # Create a connection to the MySQL database
 db_conn = connections.Connection(
@@ -26,32 +31,30 @@ db_conn = connections.Connection(
 output = {}
 table = 'employee';
 
-# Define the supported color codes
-color_codes = {
-    "red": "#e74c3c",
-    "green": "#16a085",
-    "blue": "#89CFF0",
-    "blue2": "#30336b",
-    "pink": "#f4c2c2",
-    "darkblue": "#130f40",
-    "lime": "#C1FF9C",
-}
+s3_client = boto3.client('s3')
 
+imagesDir = "static" 
 
-# Create a string of supported colors
-SUPPORTED_COLORS = ",".join(color_codes.keys())
+if not os.path.exists(imagesDir):
+    os.makedirs(imagesDir)
 
-# Generate a random color
-COLOR = random.choice(["red", "green", "blue", "blue2", "darkblue", "pink", "lime"])
-
-
+def download(bucket=bucketname, imageName=bgimage):
+    bgImagePath = os.path.join(imagesDir, imageName)
+    try:
+        s3_client.download_file(bucket, imageName, bgImagePath)
+        print(f"Successfully downloaded {imageName} from bucket {bucket} to {bgImagePath}")
+        return os.path.join("/", imagesDir, imageName)
+    except Exception as e:
+        print(f"Exception occurred while fetching the image from S3: {e}")
+        return None
+       
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template('addemp.html', color=color_codes[COLOR])
+    return render_template('addemp.html', image=image_url, group_name=groupName)
 
 @app.route("/about", methods=['GET','POST'])
 def about():
-    return render_template('about.html', color=color_codes[COLOR])
+    return render_template('about.html', image=image_url, group_name=groupName)
     
 @app.route("/addemp", methods=['POST'])
 def AddEmp():
@@ -75,11 +78,11 @@ def AddEmp():
         cursor.close()
 
     print("all modification done...")
-    return render_template('addempoutput.html', name=emp_name, color=color_codes[COLOR])
+    return render_template('addempoutput.html', name=emp_name, image=image_url, group_name=groupName)
 
 @app.route("/getemp", methods=['GET', 'POST'])
 def GetEmp():
-    return render_template("getemp.html", color=color_codes[COLOR])
+    return render_template("getemp.html", image=image_url, group_name=groupName)
 
 
 @app.route("/fetchdata", methods=['GET','POST'])
@@ -94,7 +97,6 @@ def FetchData():
         cursor.execute(select_sql,(emp_id))
         result = cursor.fetchone()
         
-        # Add No Employee found form
         output["emp_id"] = result[0]
         output["first_name"] = result[1]
         output["last_name"] = result[2]
@@ -108,29 +110,9 @@ def FetchData():
         cursor.close()
 
     return render_template("getempoutput.html", id=output["emp_id"], fname=output["first_name"],
-                           lname=output["last_name"], interest=output["primary_skills"], location=output["location"], color=color_codes[COLOR])
+                           lname=output["last_name"], interest=output["primary_skills"], location=output["location"], image=image_url, group_name=groupName)
 
 if __name__ == '__main__':
-    
-    # Check for Command Line Parameters for color
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--color', required=False)
-    args = parser.parse_args()
-
-    if args.color:
-        print("Color from command line argument =" + args.color)
-        COLOR = args.color
-        if COLOR_FROM_ENV:
-            print("A color was set through environment variable -" + COLOR_FROM_ENV + ". However, color from command line argument takes precendence.")
-    elif COLOR_FROM_ENV:
-        print("No Command line argument. Color from environment variable =" + COLOR_FROM_ENV)
-        COLOR = COLOR_FROM_ENV
-    else:
-        print("No command line argument or environment variable. Picking a Random Color =" + COLOR)
-
-    # Check if input color is a supported one
-    if COLOR not in color_codes:
-        print("Color not supported. Received '" + COLOR + "' expected one of " + SUPPORTED_COLORS)
-        exit(1)
-
-    app.run(host='0.0.0.0',port=8080,debug=True)
+    bgimage_url = download()  
+    print(bgimage_url)
+    app.run(host='0.0.0.0', port=81, debug=True)
